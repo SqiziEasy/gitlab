33bf4e1c1107661a3475ba0c8a3bef24d92c1dd9
SqiziEasy <danila.goro@yandex.ru>
2023-04-19 23:06:27 +0300

    комментарий

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//комментарий
 int main()
 {
     int a, b;
@@ -11,8 +12,5 @@ int main()
 
     printf("сумма двух веденных чисел = %d\n", a+b);
 
-    printf("разность двух веденных чисел = %d\n", a-b);
-
-
-    return 1;
+    return 0;
 }
