f72b28e37a93c21f93b2b1115bc57e11df0eba88
SqiziEasy <danila.goro@yandex.ru>
2023-04-19 22:08:15 +0300

    hello, world!

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Введите первое целое число: ");
+    scanf("%d", &a);
+    printf("Введите второе целое число: ");
+    scanf("%d", &b);
+
+
+    return 0;
+}
